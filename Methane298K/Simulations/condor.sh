982d5a1da559150bc37bf7ea1cff29f2270e780e
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-02-03 09:45:14 -0500

    Create condor.sh

---
+++ Methane298K/Simulations/condor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+universe= vanilla
+output= output.txt
+error= errors.txt
+should_transfer_files=yes
+when_to_transfer_output=on_exit
+transfer_input_files=simulation.input,CCC.cif
+log= logfile
+executable=/scratch365/eosaro/Research/CoreMOFs/MPNN_CoRE-ASR/CH4/AllSimulations/FFF/INDEX/raspa.sh
+queue
